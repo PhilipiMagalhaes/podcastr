b3ff9bb3c8321b980dd734ec3c0b3495051e0516
philipimagalhaes <philipimagalhaes@gmail.com>
2021-04-21 22:51:12 -0300

    typed return from api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,47 @@
-import api from '../src/services/api';
+import { GetStaticProps } from "next";
+import api from "../src/services/api";
 
-export default function Home(props) {
-  console.log(props.episodes);
+type File = {
+    url: String;
+    type: String;
+    duration: Number;
+};
+type Episode = {
+  id: String;
+  title: String;
+  members: String;
+  published_at: String;
+  thumbnail: String;
+  description: String;
+  file: File;
+};
+type HomeProps = {
+  episodes: Episode[];
+};
+
+export default function Home(props: HomeProps) {
   return (
-   <>
-   <span>''</span>
-   </>
+    <>
+      <span>''</span>
+    </>
   );
 }
 
-export async function getStaticProps() {
-   const episodes = await api.get("/episodes");
-   console.log(episodes);
+export const getStaticProps: GetStaticProps = async () => {
+  const queryParams = {
+    _limit: 12,
+    _sort: 'published_at',
+    _order: 'desc',
+  };
+  const episodes = await api.get('/episodes',{
+    params:queryParams,
+  });
+  console.log(episodes);
 
-   return {
-     props: {
-       episodes: episodes.data,
-     },
-     revalidate: 60*60*8,
-   }   
+  return {
+    props: {
+      episodes: episodes.data
+    },
+    revalidate: 60 * 60 * 8
+  };
 }
